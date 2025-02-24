808ac32ae7e5e4c7732131dff42f7cf4ca2b65d1
evanskfchikuni <vance04real@gmail.com>
2025-02-24 21:20:06 +0200

    initial commit

---
+++ src/test/java/zw/co/opticode/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.opticode.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
